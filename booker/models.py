e540e555846336e0a6d65ad375830e18ddee859e
guilhermeslucas <guilherme.slucas@gmail.com>
2018-08-27 01:04:41 +0000

    first commit

---
+++ booker/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    level = models.IntegerField()
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    begin = models.DateTimeField('begin date')
+    end = models.DateTimeField('end date')
