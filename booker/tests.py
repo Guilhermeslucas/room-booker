74ad5f0fd3f279f41412111cb4b3feb51a53c5af
guilhermeslucas <guilherme.slucas@gmail.com>
2018-09-07 20:45:18 +0000

    adding more working tests

--- booker/tests.py
+++ booker/tests.py
@@ -10,7 +10,7 @@ class bookerTest(TestCase):
         response = client.post('/booker/rooms/', post_data)
         post_data['pk'] = 1
         self.assertEqual(post_data, response.json())
-    
+
     def test_create_meeting_valid_room(self):
         post_data = {'name': 'quaresma','level': 1,'description': 'large'}
         Room.objects.create(**post_data)
@@ -36,8 +36,18 @@ class bookerTest(TestCase):
         self.assertEqual(response.json(), [room_data])
 
     def test_list_meetings(self):
-        pass
+        room_data = {'name': 'quaresma','level': 1,'description': 'large'}
+        Room.objects.create(**room_data)
 
+        post_data =  {'begin': '2018-04-10 20:00:00','end': '2018-04-10 21:00:00','title': 'luizalabs meeting','room_pk': 1}
+        response_room = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+        
+        self.assertEqual(201, response_room.status_code)
+
+        response_meeting_list = client.get('/booker/reservations/')
+
+        print(response_meeting_list.json())
+    
     def test_change_meeting_info(self):
         pass
 
@@ -50,8 +60,8 @@ class bookerTest(TestCase):
     def test_delete_meeting(self):
         pass
 
-    def test_create_valid_meeting(self):
+    def test_two_meetings_ok(self):
         pass
-
-    def test_create_invalid_meeting(self):
+    
+    def test_two_meetings_conflict(self):
         pass
