955be6b244431c97f320b99278dfd796a65b663d
guilhermeslucas <guilherme.slucas@gmail.com>
2018-09-07 22:25:21 +0000

    adding more working tests. just have to test patch methods

--- booker/tests.py
+++ booker/tests.py
@@ -45,8 +45,9 @@ class bookerTest(TestCase):
         self.assertEqual(201, response_room.status_code)
 
         response_meeting_list = client.get('/booker/reservations/')
-
-        print(response_meeting_list.json())
+        expected_response = [{'end': '2018-04-10T21:00:00Z', 'title': 'luizalabs meeting', 'begin': '2018-04-10T20:00:00Z', 'pk': 1, 'room': {'description': 'large', 'level': 1, 'id': 1, 'name': 'quaresma'}}]
+        
+        self.assertEqual(response_meeting_list.json(), expected_response)
     
     def test_change_meeting_info(self):
         pass
@@ -55,13 +56,62 @@ class bookerTest(TestCase):
         pass
 
     def test_delete_room(self):
-        pass
-    
+        post_data = {'name': 'quaresma','level': 1,'description': 'large'}
+        Room.objects.create(**post_data)
+
+        response_room = client.delete('/booker/rooms/1/')
+
+        self.assertEqual(200, response_room.status_code)
+
+        response_after_deleted = client.get('/booker/rooms/')
+
+        self.assertEqual(response_after_deleted.json(), [])
+
+    def test_delete_room_not_exits(self):
+        response_room = client.delete('/booker/rooms/1/')
+        
+        self.assertEqual(404, response_room.status_code)
+
     def test_delete_meeting(self):
-        pass
+        room_data = {'name': 'quaresma','level': 1,'description': 'large'}
+        Room.objects.create(**room_data)
+
+        post_data =  {'begin': '2018-04-10 20:00:00','end': '2018-04-10 21:00:00','title': 'luizalabs meeting','room_pk': 1}
+        response_room = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+
+        response_after_deleted = client.delete('/booker/reservations/1/')
+
+        self.assertEqual(response_after_deleted.status_code, 200)
+
+        response_list_meetings = client.get('/booker/reservations/')
+
+        self.assertEqual([], response_list_meetings.json())
+
+    def test_delete_meeting_not_exists(self):
+        response_after_deleted = client.delete('/booker/reservations/1/')
+
+        self.assertEqual(404, response_after_deleted.status_code)
 
     def test_two_meetings_ok(self):
-        pass
+        room_data = {'name': 'quaresma','level': 1,'description': 'large'}
+        Room.objects.create(**room_data)
+
+        post_data =  {'begin': '2018-04-10 20:00:00','end': '2018-04-10 21:00:00','title': 'luizalabs meeting','room_pk': 1}
+        response_first_reservation = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+
+        post_data =  {'begin': '2018-04-10 21:00:00','end': '2018-04-10 22:00:00','title': 'luizalabs meeting','room_pk': 1}
+        response_second_reservation = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+
+        self.assertEqual(response_second_reservation.status_code, 201)
     
     def test_two_meetings_conflict(self):
-        pass
+        room_data = {'name': 'quaresma','level': 1,'description': 'large'}
+        Room.objects.create(**room_data)
+
+        post_data =  {'begin': '2018-04-10 20:00:00','end': '2018-04-10 21:00:00','title': 'luizalabs meeting','room_pk': 1}
+        response_first_reservation = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+
+        post_data =  {'begin': '2018-04-10 20:00:00','end': '2018-04-10 20:30:00','title': 'luizalabs meeting','room_pk': 1}
+        response_second_reservation = client.post('/booker/reservations/', json.dumps(post_data), content_type="application/json")
+
+        self.assertEqual(response_second_reservation.status_code, 409)
